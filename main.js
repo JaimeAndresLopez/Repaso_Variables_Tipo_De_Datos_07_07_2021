eea92cd76e217a6632c901d473e4d3fcc195027b
JaimeAndresLopez <james_2005_scott@hotmail.coml>
2021-07-07 20:57:42 -0500

    Repaso de variables y tipo de datos 07 07 2021

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log(`Hola Mundo`);
+nombreEstudiante = `Jaime Lopez`;
+miEdad = 28;
+estatura = 1.87;
+soyUnProfesor = false;
+hobbies = [`Video Juegos`, `Ultimate Frisbee`, `Programar`]
+
+console.log(nombreEstudiante, typeof(nombreEstudiante));
+console.log(miEdad, typeof(miEdad));
+alert(`Mi altura es ${estatura} cm`);
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+console.log(hobbies);
